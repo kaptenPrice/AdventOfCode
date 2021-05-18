a111927a9a5c459676822b380584cb8698313b6e
kaptenPrice <khazar.mehraban@gmail.com>
2021-05-18 12:25:50 +0200

    Changed fetchData to local, removed backend

--- frontend/src/components/KnightOfTheDinnerTable.js
+++ frontend/src/components/KnightOfTheDinnerTable.js
@@ -1,36 +1,38 @@
 import React, { useEffect, useState } from 'react'
+import input from '../input.text'
 
 const KnightOfTheDinnerTable = () => {
 	const [data, setData] = useState('')
+	// console.log(input.toString())
 
 	useEffect(() => {
 		fetchData()
-		createObject()
 	}, [data])
+  const stringArray = data.match(/(\b[A-Z].*?\b)|(gain|lose)|([0-9]+)/gm)
+
 
 	const fetchData = async () => {
 		try {
-			const res = await fetch('http://localhost:3008/')
-			const data = await res.json()
-			setData(data.inputData)
+			const res = await fetch(input)
+			const data = await res.text()
+			setData(data)
 		} catch (error) {
 			console.log('error, ', error)
 		}
 	}
-	const createObject = () => {
-		const stringArray = data.match(/(\b[A-Z].*?\b)|(gain|lose)|([0-9]+)/gm)
-		const objectData = {}
-		if (stringArray?.length) {
-			for (let i = 0; i < stringArray.length; i += 4) {
-				//Creating every person
-				objectData[stringArray[i]] = { ...objectData[stringArray[i]] }
-				//Creating every persons preferences
-				objectData[stringArray[i]][stringArray[i + 3]] =
-					(stringArray[i + 1] === 'lose' ? -1 : 1) * stringArray[i + 2]
-			}
-			console.log(objectData)
+
+	console.log(stringArray)
+	const objectData = {}
+	if (stringArray?.length) {
+		for (let i = 0; i < stringArray.length; i += 4) {
+			//Creating every guest
+			const hapinessValue = (stringArray[i + 1] === 'lose' ? -1 : 1) * stringArray[i + 2]
+			//Creating every guests neighbors and value of happiness
+			objectData[stringArray[i]] = { ...objectData[stringArray[i]], [stringArray[i + 3]]: hapinessValue }
 		}
+		console.log(objectData)
 	}
+  
 
 	return (
 		<div
@@ -42,7 +44,7 @@ const KnightOfTheDinnerTable = () => {
 			}}
 		>
 			<h2> --- Day 13: Knights of the Dinner Table ---</h2>
-			<div></div>
+			<div>{Object.keys(objectData).map((e)=><p>{e}</p>)}</div>
 		</div>
 	)
 }
